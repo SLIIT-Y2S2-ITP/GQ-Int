bd66daf91002f06b8d059e7fea1a634b4547ec8b
“senarathnaPP” <it20204020@my.sliit.lk>
2021-08-22 22:53:01 +0530

    updated Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -11,38 +11,6 @@ import GQ1 from '../images/gq-logo2.png'
 const FooterPage = () => {
   return (
 
-    /*<div className='Footer1'>
-      <table border='3px'>
-        <td>
-        initial update 34567890
-          <th id='Fc1'>
-            <Link to= '/' className='navbar-log'>
-          <img src={GQ1} height='200px' width='400px;' alt="GQ logo" className="gq-logo-footer"/>
-          </Link>
-          </th>
-        </td>
-
-        <td>
-          <th id='Fc2'>
-          <i className='fab fa-facebook' id='fb' style={{color:"#34b7f1"}} ></i>
-            <i className='fab fa-whatsapp' id='fb' style={{color:"#25d366"}}></i>
-            <i className='fab fa-viber' id='fb' style={{color:"#B026FF"}}></i>
-            <h5 className='GQcontact'>Contact: 072 778 7788</h6><br/>
-           <h6 className='GQcontactE'>Email: contactgqintl@gmail.com</h6>
-          </th>
-        </td>
-
-        <td>
-          <th>
-          <h6 className='address-1'>231/10 2/3</h6><br/>
-        <h6 className='address-2'>Panchikawatta Rd,</h6><br/>
-        <h6 className='address-3'>Colombo 10</h6><br/>
-          </th>
-        </td>
-
-      </table>
-       </div>
-       */
     <div className='Footer1'><br/>
       <table className='tablefoot'>
         <tr >
